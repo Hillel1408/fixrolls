203f5b1b3d0e78714117c2fefa3e344d4f317058
Hillel1408 <etemax@bk.ru>
2023-11-18 12:42:31 +0300

    feat: верстка главной

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,109 +3,115 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="bg-[#F2F2F2]">
-      <div className="max-w-[1117px] mx-auto px-[10px] pt-[34px] pb-[121px] grid grid-cols-[641px_255px] justify-between">
-        <div>
-          <div className="flex gap-[26px] items-center mb-10">
-            <img src="/images/logo-2.png" alt="" />
+      <div className="container grid grid-cols-[232px_1fr_334px]">
+        <div></div>
 
-            <p className="text-[#21201F] text-[16px] font-medium max-w-[202px]">
-              Доставка роллов и суши в Нижнем Новгороде
+        <div className="w-full gap-5 mx-auto px-[10px] pt-[34px] pb-[121px] grid grid-cols-[641px_255px] justify-between ">
+          <div>
+            <div className="flex gap-[26px] items-center mb-10">
+              <img src="/images/logo-2.png" alt="" />
+
+              <p className="text-[#21201F] text-[16px] font-medium max-w-[202px]">
+                Доставка роллов и суши в Нижнем Новгороде
+              </p>
+            </div>
+
+            <div className="flex justify-between mb-11 text-[rgba(33,32,31,0.82)] text-[16px] font-medium">
+              <ul className="max-w-[290px] flex flex-col gap-[14px]">
+                <li>
+                  <Link to="/">
+                    Пользовательское соглашение об условиях доставки
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/">Состав и калорийность</Link>
+                </li>
+                <li>
+                  <Link to="/">Политика конфиденциальности</Link>
+                </li>
+              </ul>
+
+              <ul className="max-w-[290px] flex flex-col gap-[14px]">
+                <li>
+                  <Link to="/">Правила бонусной системы</Link>
+                </li>
+                <li>
+                  <Link to="/">Франшиза</Link>
+                </li>
+                <li>
+                  <Link to="/">Контакты</Link>
+                </li>
+              </ul>
+            </div>
+
+            <p className="text-[#21201F] text-[16px] font-medium mb-14">
+              FixRolls - это ресторан доставки роллов и суши с нестандартным
+              подходом к своему делу. Мы предлагаем нереальные скидки и любим
+              дарить подарки.
+            </p>
+
+            <p className="text-[#21201F] text-[16px] font-medium">
+              © 2023 Все права защищены
             </p>
           </div>
 
-          <div className="flex justify-between mb-11 text-[rgba(33,32,31,0.82)] text-[16px] font-medium">
-            <ul className="max-w-[290px] flex flex-col gap-[14px]">
+          <div>
+            <button className="text-[#21201F] text-[16px] border-b border-[#21201F] mb-11">
+              Зоны доставки
+            </button>
+
+            <div className="flex flex-col gap-6 mb-[82px]">
+              <div className="flex flex-col">
+                <span className="text-[#969696] text-[24px] font-medium">
+                  Заказать
+                </span>
+                <Link
+                  className="text-[#21201F] text-[26px] font-semibold"
+                  to="tel:+7(800)777-98-65"
+                >
+                  +7 (800) 777-98-65
+                </Link>
+              </div>
+
+              <div className="flex flex-col">
+                <span className="text-[#969696] text-[24px] font-medium">
+                  Для отзывов
+                </span>
+                <Link
+                  className="text-[#21201F] text-[26px] font-semibold"
+                  to="mailto:reviews@rollsfix.ru"
+                >
+                  reviews@rollsfix.ru
+                </Link>
+              </div>
+            </div>
+
+            <ul className="flex gap-2">
               <li>
-                <Link to="/">
-                  Пользовательское соглашение об условиях доставки
+                <Link
+                  to="#"
+                  className="w-[66px] h-[66px] bg-white flex justify-center items-center rounded-[20px]"
+                >
+                  <svg className="w-5 h-5" aria-hidden="true">
+                    <use xlinkHref="/sprites/sprite.svg#telegram"></use>
+                  </svg>
                 </Link>
               </li>
               <li>
-                <Link to="/">Состав и калорийность</Link>
-              </li>
-              <li>
-                <Link to="/">Политика конфиденциальности</Link>
-              </li>
-            </ul>
-
-            <ul className="max-w-[290px] flex flex-col gap-[14px]">
-              <li>
-                <Link to="/">Правила бонусной системы</Link>
-              </li>
-              <li>
-                <Link to="/">Франшиза</Link>
-              </li>
-              <li>
-                <Link to="/">Контакты</Link>
+                <Link
+                  to="#"
+                  className="w-[66px] h-[66px] bg-white flex justify-center items-center rounded-[20px]"
+                >
+                  <svg className="w-5 h-5" aria-hidden="true">
+                    <use xlinkHref="/sprites/sprite.svg#vk"></use>
+                  </svg>
+                </Link>
               </li>
             </ul>
           </div>
-
-          <p className="text-[#21201F] text-[16px] font-medium mb-14">
-            FixRolls - это ресторан доставки роллов и суши с нестандартным
-            подходом к своему делу. Мы предлагаем нереальные скидки и любим
-            дарить подарки.
-          </p>
-
-          <p className="text-[#21201F] text-[16px] font-medium">
-            © 2023 Все права защищены
-          </p>
         </div>
 
-        <div>
-          <button className="text-[#21201F] text-[16px] border-b border-[#21201F] mb-11">
-            Зоны доставки
-          </button>
-
-          <div className="flex flex-col gap-6 mb-[82px]">
-            <div className="flex flex-col">
-              <span className="text-[#969696] text-[24px] font-medium">
-                Заказать
-              </span>
-              <Link
-                className="text-[#21201F] text-[26px] font-semibold"
-                to="tel:+7(800)777-98-65"
-              >
-                +7 (800) 777-98-65
-              </Link>
-            </div>
-
-            <div className="flex flex-col">
-              <span className="text-[#969696] text-[24px] font-medium">
-                Для отзывов
-              </span>
-              <Link
-                className="text-[#21201F] text-[26px] font-semibold"
-                to="mailto:reviews@rollsfix.ru"
-              >
-                reviews@rollsfix.ru
-              </Link>
-            </div>
-          </div>
-
-          <ul className="flex gap-2">
-            <li>
-              <Link
-                to="#"
-                className="w-[66px] h-[66px] bg-white flex justify-center items-center rounded-[20px]"
-              >
-                <svg className="w-5 h-5" aria-hidden="true">
-                  <use xlinkHref="/sprites/sprite.svg#telegram"></use>
-                </svg>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="#"
-                className="w-[66px] h-[66px] bg-white flex justify-center items-center rounded-[20px]"
-              >
-                <svg className="w-5 h-5" aria-hidden="true">
-                  <use xlinkHref="/sprites/sprite.svg#vk"></use>
-                </svg>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <div></div>
       </div>
     </footer>
   );
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,13 @@
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import { useState } from "react";
 
 const Main = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const navItems = [
     "Комбо",
     "Сеты по 599₽",
@@ -16,10 +22,30 @@ const Main = () => {
     "Соусы и специи",
   ];
 
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    mode: "free",
+
+    slides: {
+      spacing: 8,
+      perView: 3.7,
+    },
+
+    breakpoints: {},
+
+    created() {
+      setLoaded(true);
+    },
+
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+  });
+
   return (
     <div className="container">
-      <div className="grid grid-cols-[232px_1fr_334px] pt-4 rounded-2xl items-start">
-        <div className="bg-white pt-6 px-2 pb-2 rounded-2xl">
+      <div className="grid grid-cols-[232px_1fr_334px] pt-4 rounded-2xl items-start gap-[26px]">
+        <div className="bg-white pt-6 px-2 pb-2 rounded-2xl sticky top-0">
           <h2 className="text-[#21201F] text-[22px] font-medium px-[16px] mb-[13px]">
             Меню
           </h2>
@@ -38,9 +64,71 @@ const Main = () => {
           </ul>
         </div>
 
-        <div></div>
+        <div className="overflow-hidden">
+          <div className="mb-[37px] px-[10px] py-2 bg-white rounded-[30px]">
+            <div
+              ref={sliderRef}
+              className="keen-slider max-w-[1126px] rounded-[30px]"
+            >
+              {new Array(8).fill("").map((item, index) => (
+                <div className="keen-slider__slide">
+                  <img
+                    className="h-[285px] object-cover w-full rounded-[20px]"
+                    src="/images/slide-1.jpg"
+                    alt=""
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
 
-        <div className="bg-white pt-6 px-6 pb-2 rounded-2xl flex flex-col gap-[232px]">
+          <div className="flex flex-col gap-[50px]">
+            {new Array(4).fill("").map((item, index) => (
+              <div key={index}>
+                <h2 className="text-[#21201F] text-[30px] font-medium mb-4">
+                  Сеты по 599₽
+                </h2>
+                <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] gap-x-2 gap-y-4">
+                  {new Array(7).fill("").map((item, index) => (
+                    <div
+                      key={index}
+                      className="px-2 pt-[6px] pb-[15px] bg-white rounded-[21px] flex flex-col"
+                    >
+                      <img
+                        className="h-[195px] object-cover w-full"
+                        src="/images/img-2.jpg"
+                        alt=""
+                      />
+
+                      <div className="px-4 flex-auto">
+                        <h3 className="text-[#21201F] text-[18px] font-medium">
+                          SET Горячий
+                        </h3>
+                        <p className="text-[#797979] text-[12px] font-medium">
+                          5 микс ролов / 40 шт
+                        </p>
+                        <div className="flex gap-2 items-center mt-6">
+                          <span className="text-[#21201F] text-[22px] font-medium">
+                            599₽
+                          </span>
+                          <span className="text-[#A5A5A5] text-[15px] line-through">
+                            1045₽
+                          </span>
+                        </div>
+                      </div>
+
+                      <button className="h-12 w-full rounded-2xl bg-[#F2F2F2] text-[#21201F] text-[16px] font-medium mt-[10px]">
+                        Добавить
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="bg-white pt-6 px-6 pb-2 rounded-2xl flex flex-col gap-[232px] sticky top-0">
           <h2 className="text-[#21201F] text-[26px] font-medium mb-[13px]">
             Корзина
           </h2>
