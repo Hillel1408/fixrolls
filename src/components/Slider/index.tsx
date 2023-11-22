3bfa5cf6d4084854597d4f8709ba00ceae0ee465
Hillel1408 <etemax@bk.ru>
2023-11-22 22:09:23 +0300

    feat: добавил картинки слайдера

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -6,6 +6,8 @@ const Slider = () => {
     const [loaded, setLoaded] = useState(false);
     const [currentSlide, setCurrentSlide] = useState(0);
 
+    const images = ["slide-1.jpg", "slide-2.jpg", "slide-3.jpg", "slide-4.jpg"];
+
     const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
         mode: "free",
 
@@ -28,11 +30,14 @@ const Slider = () => {
     return (
         <div className="mb-[37px] px-[10px] py-2 bg-white rounded-[30px] sm:rounded-none sm:mb-0">
             <div ref={sliderRef} className="keen-slider max-w-[1126px] rounded-[30px]">
-                {new Array(8).fill("").map((item, index) => (
-                    <div className="keen-slider__slide !min-w-[285px] sm:!min-w-[207px]">
+                {images.map((item, index) => (
+                    <div
+                        key={index}
+                        className="keen-slider__slide !min-w-[285px] sm:!min-w-[207px]"
+                    >
                         <img
                             className="h-[285px] object-cover w-full rounded-[20px] sm:h-[207px]"
-                            src="/images/slide-1.jpg"
+                            src={`/images/${item}`}
                             alt=""
                         />
                     </div>
