1fc81ec094ae4f1e8387845a146b686d4989a1b6
Hillel1408 <etemax@bk.ru>
2023-11-23 10:45:16 +0300

    fix

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,11 +2,11 @@ import { Header, Footer } from "components";
 
 const Layout = ({ children }: { children: any }) => {
     return (
-        <div className="w-full">
+        <>
             <Header />
             <main className="bg-[#F2F2F2]">{children}</main>
             <Footer />
-        </div>
+        </>
     );
 };
 
