743c6f4a9bec1e77b4f9530754bd8d2cc16457b7
Hillel1408 <etemax@bk.ru>
2023-11-24 00:20:16 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main, Order } from "pages";
 import { Layout } from "components";
 import { ROUTES } from "constants/";
-import { useEffect } from "react";
-import { useMatchMedia } from "hooks";
 
 function App() {
-    const { isMobile, isTablet, isDesktop } = useMatchMedia();
-
-    useEffect(() => {
-        const a = document.querySelector("meta[name=viewport]");
-
-        a?.setAttribute(
-            "content",
-            isTablet ? "width=1365, initial-scale=1" : "width=device-width, initial-scale=1",
-        );
-    });
-
     return (
         <Router>
             <Layout>
