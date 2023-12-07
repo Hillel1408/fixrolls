44a26383d05b769824915019d66b31f205fcdc39
Hillel1408 <etemax@bk.ru>
2023-12-07 19:56:25 +0300

    FixRolls

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main, Order } from "pages";
 import { ROUTES } from "constants/";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+        window.addEventListener("resize", (e) => {
+            setTimeout(() => {
+                const mvp = document.getElementById("myViewport");
+
+                window.screen.width > 479
+                    ? mvp?.setAttribute("content", "width=1365")
+                    : mvp?.setAttribute("content", "width=device-width, initial-scale=1");
+            }, 300);
+        });
+    }, []);
+
     return (
         <Router>
             <Routes>
